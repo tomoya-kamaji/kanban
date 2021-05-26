9b58ebe08a2ad88fe0b3ed8c0435a372aa511a22
tomo_kamaji <tomoya.kamaji@gmail.com>
2021-05-26 09:45:28 +0900

    カード作成、詳細閲覧、削除機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root "top#index"
-  resources :list, only: %i(new create)
+
+  resources :list, only: %i(new create edit update destroy) do
+    resources :card, only: %i(new create show)
+    resources :card, only: %i(new create show edit update)
+  end
 end
